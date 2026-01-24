c5097b7b9988cbbb8eab605c255c5d19d495a6ad
JoaquinECB <jcaceresbello@byupathway.edu>
2026-01-24 17:30:56 -0500

    add admin

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -1,2 +1,6 @@
 INSERT INTO account (account_firstname, account_lastname, account_email, account_password)
 VALUES ('Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n');
+
+UPDATE account 
+SET account_type = 'Admin' 
+WHERE account_email = 'tony@starkent.com';
