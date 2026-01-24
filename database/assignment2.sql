5fe23d49eabb54b9ba87cebd2ac41dc0ad3ebdce
JoaquinECB <jcaceresbello@byupathway.edu>
2026-01-24 17:33:20 -0500

    add update

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -7,3 +7,7 @@ WHERE account_email = 'tony@starkent.com';
 
 DELETE FROM account 
 WHERE account_email = 'tony@starkent.com';
+
+UPDATE inventory 
+SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior') 
+WHERE inv_make = 'GM' AND inv_model = 'Hummer';
