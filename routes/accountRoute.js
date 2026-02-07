c865a4e57bb559fbc8d612f6d3a075419f64ea3f
JoaquinECB <jcaceresbello@byupathway.edu>
2026-02-07 01:21:56 -0500

    add server-side

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -18,5 +18,15 @@ router.post(
   utilities.handleErrors(accountController.registerAccount)
 )
 
+// Process the login attempt
+router.post(
+  "/login",
+  regValidate.loginRules(),
+  regValidate.checkLoginData,
+  (req, res) => {
+    res.status(200).send('login process')
+  }
+)
+
 // Export the router
 module.exports = router
