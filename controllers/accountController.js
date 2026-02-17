3700877c6cbf57015410dfd520e25fa057e5345f
JoaquinECB <jcaceresbello@byupathway.edu>
2026-02-17 00:07:08 -0500

    fixing accounts

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -97,7 +97,7 @@ async function accountLogin(req, res) {
       delete accountData.account_password
       const accessToken = jwt.sign(accountData, process.env.ACCESS_TOKEN_SECRET, { expiresIn: 3600 * 1000 })
       if(process.env.NODE_ENV === 'development') {
-        res.cookie("jwt", accessToken, { httpOnly: true, maxAge: 3600 * 1000 })
+        res.cookie("jwt", accessToken, { httpOnly: false, maxAge: 3600 * 1000 })
       } else {
         res.cookie("jwt", accessToken, { httpOnly: true, secure: true, maxAge: 3600 * 1000 })
       }
